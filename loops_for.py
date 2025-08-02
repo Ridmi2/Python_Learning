3661697936a53c40b2d7377b19d066af4c5c62ee
Ridmi Tharaka Wickramasinghe <ridmit12@gmail.com>
2025-08-02 17:14:22 +0530

    loops-for,while

---
+++ loops_for.py
@@ -0,0 +1,24 @@
+x =[1,2,3,4,5]
+for num in x:
+    print(num)
+
+
+y={'name':'Ridmi','age':25,'city':'Moratuwa'}
+for i in y.keys():
+    print(i)   
+
+for i in y.values():
+    print(i) 
+
+for i in y.items():#both key and value
+    print(i) 
+
+
+
+#range it is starting from 0
+for i in range(5):#0,1,2,3,4,
+    print(i)
+
+#think that ou want to collect even numbers to 10  
+for i in range(2,11,2):
+    print(i)   
---
+++ loops_while.py
@@ -0,0 +1,8 @@
+#
+i=1
+sum=0
+while(i<6):
+    number = int(input("Enter a number:"))
+    sum = sum+number
+    i+= 1
+print("Sum of numbers:", sum)
